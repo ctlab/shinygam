5a6b22e46d1dc9cf43d2a6a138a5b37eaf92f25f
Alexey Sergushichev <alsergbox@gmail.com>
2015-12-17 13:51:03 +0300

    Using local copy of svg-pan-zoom II

--- ui.R
+++ ui.R
@@ -166,7 +166,7 @@ aboutPanel <- fixedRow(
 shinyUI(
     fluidPage(
         tags$head(
-        includeScript("svg-pan-zoom.min.js"),
+        tags$script(srt="svg-pan-zoom.min.js"),
         tags$script(src="d3.v3.min.js"),
         #tags$script(src="d3.v3.js"),
         tags$script(src="gam.js"),
